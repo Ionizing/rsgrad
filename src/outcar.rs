5897a63f84f865b0cb336b6462a038631aeffbbf
Ionizing <ljchen9@mail.ustc.edu.cn>
2021-01-30 19:03:36 +0800

    [outcar.rs] ISPIN NIONS POSITIONS FORCES CPUTIME ENERGY parser done

---
+++ src/outcar.rs
@@ -0,0 +1,323 @@
+type MatX3<T> = Vec<[T;3]>;  // Nx3 matrix
+type Mat33<T> = [[T;3];3];   // 3x3 matrix
+
+use rayon::prelude;
+use regex::Regex;
+
+
+// DONE ISPIN
+// TODO ions per type
+// TODO element symbol
+// TODO NKPTS
+// TODO stress
+// TODO cell
+// DONE positions and forces
+// TODO magmom
+// TODO E-fermi
+// TODO scf
+// TODO viberation
+
+
+#[derive(Clone)]
+struct IonicIteration {
+    nscf      : i32,
+    toten     : f64,
+    toten_z   : f64,
+    cputime   : f64,
+    magmom    : f64,
+    positions : MatX3<f64>,
+    forces    : MatX3<f64>,
+    cell      : Mat33<f64>,
+}
+
+
+#[derive(Clone)]
+struct Viberation {
+    freq   : f64,  // in THz
+    dxdydz : MatX3<f64>,
+}
+
+
+#[derive(Clone)]
+struct Outcar {
+    ispin  : i32,
+    nions  : i32,
+    efermi : f64,
+    cell   : Mat33<f64>,
+    vib    : Option<Viberation>,
+}
+
+
+impl Outcar {
+    fn parse_ispin(context: &str) -> i32 {
+        Regex::new(r"ISPIN  =      (\d)")
+            .unwrap()
+            .captures(context)
+            .unwrap()
+            .get(1)
+            .unwrap()
+            .as_str()
+            .parse::<i32>()
+            .unwrap()
+    }
+
+    fn parse_nions(context: &str) -> i32 {
+        Regex::new(r"NIONS = \s+(\d+)")
+            .unwrap()
+            .captures(context)
+            .unwrap()
+            .get(1)
+            .unwrap()
+            .as_str()
+            .parse::<i32>()
+            .unwrap()
+    }
+
+    fn parse_toten(context: &str) -> Vec<f64> {
+        Regex::new(r"free  energy   TOTEN  = \s*([-+]?[0-9]+[.]?[0-9]*([eE][-+]?[0-9]+)?) eV")
+            .unwrap()
+            .captures_iter(context)
+            .map(|x| {
+                x.get(1)
+                 .unwrap()
+                 .as_str()
+                 .parse::<f64>()
+                    .unwrap()
+            })
+            .collect()
+    }
+
+    fn parse_toten_z(context: &str) -> Vec<f64> {
+        Regex::new(r"energy  without entropy=\s+([-+]?[0-9]+[.]?[0-9]*?)  energy\(sigma->0\) =\s+([-+]?[0-9]+[.]?[0-9]*)")
+            .unwrap()
+            .captures_iter(context)
+            // .inspect(|x| {dbg!(&x);})
+            .map(|x| {
+                x.get(2)
+                 .unwrap()
+                 .as_str()
+                 .parse::<f64>()
+                    .unwrap()
+            })
+            .collect()
+    }
+
+    fn parse_cputime(context: &str) -> Vec<f64> {
+        Regex::new(r"LOOP\+:  cpu time .* real time \s+([0-9]+[.]?[0-9]*)")
+            .unwrap()
+            .captures_iter(context)
+            .map(|x| {
+                x.get(1)
+                 .unwrap()
+                 .as_str()
+                 .parse::<f64>()
+                    .unwrap()
+            })
+            .collect()
+    }
+
+    fn parse_magmom(context: &str, is_ncl: bool) -> Option<Vec<f64>> {
+        todo!();
+    }
+
+    fn parse_posforce(context: &str) -> (Vec<MatX3<f64>>, Vec<MatX3<f64>>) {
+        Regex::new(r"(?m)^ POSITION")
+            .unwrap()
+            .find_iter(context)
+            .map(|x| x.start())
+            .map(|x| {
+                Self::_parse_posforce_single_iteration(&context[x..])
+            })
+            .fold((vec![], vec![]), |mut acc, (p, f)| {
+                acc.0.push(p);
+                acc.1.push(f);
+                acc
+            })
+    }
+
+    fn _parse_posforce_single_iteration(context: &str) -> (MatX3<f64>, MatX3<f64>) {
+        assert!(context.starts_with(" POSITION"));
+        context.lines()
+               // .inspect(|x| {dbg!(&x);})
+               .skip(2)
+               .take_while(|x| !x.starts_with(" ----"))
+               // .inspect(|x| {dbg!(&x);})
+               .map(|x| {
+                   x.split_whitespace()
+                       .map(|x| x.parse::<f64>().unwrap())
+                       .collect::<Vec<f64>>()
+               })
+               .fold((vec![], vec![]), |mut ret, x|{
+                   ret.0.push([x[0], x[1], x[2]]);
+                   ret.1.push([x[3], x[4], x[5]]);
+                   ret
+               })
+    }
+
+    fn parse_efermi(context: &str) -> f64 {
+        Regex::new(r" E-fermi : \s+([-+]?[0-9]+[.]?[0-9]*)")
+            .unwrap()
+            .captures(context)
+            .unwrap()
+            .get(1)
+            .unwrap()
+            .as_str()
+            .parse::<f64>()
+            .unwrap()
+    }
+}
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_parse_ispin() {
+        let input = r#"
+   ICHARG =      2    charge: 1-file 2-atom 10-const
+   ISPIN  =      1    spin polarized calculation?
+   LNONCOLLINEAR =      F non collinear calculations"#;
+        assert_eq!(Outcar::parse_ispin(&input), 1i32);
+    }
+
+    #[test]
+    fn test_parse_nions() {
+        let input = r#"
+   k-points           NKPTS =      1   k-points in BZ     NKDIM =      1   number of bands    NBANDS=      8
+   number of dos      NEDOS =    301   number of ions     NIONS =      4
+   non local maximal  LDIM  =      4   non local SUM 2l+1 LMDIM =      8 "#;
+        assert_eq!(Outcar::parse_nions(&input), 4i32);
+    }
+
+    #[test]
+    fn test_parse_toten() {
+        let input = r#"
+  free energy    TOTEN  =        51.95003235 eV
+  free energy    TOTEN  =       -10.91478741 eV
+  free energy    TOTEN  =       -22.11911831 eV
+  free  energy   TOTEN  =       -19.26550806 eV
+  free  energy   TOTEN  =       -19.25519593 eV
+  free  energy   TOTEN  =       -19.26817124 eV
+"#;
+        let output = vec![-19.26550806f64, -19.25519593, -19.26817124];
+        assert_eq!(Outcar::parse_toten(&input), output);
+    }
+
+    #[test]
+    fn test_parse_toten_z() {
+        let input = r#"
+  energy without entropy =       51.93837380  energy(sigma->0) =       51.94614617
+  energy without entropy =      -10.92638322  energy(sigma->0) =      -10.91865268
+  energy without entropy =      -22.13071412  energy(sigma->0) =      -22.12298358
+  energy  without entropy=      -19.27710387  energy(sigma->0) =      -19.26937333
+  energy  without entropy=      -19.26679174  energy(sigma->0) =      -19.25906120
+  energy  without entropy=      -19.27976705  energy(sigma->0) =      -19.27203651"#;
+        let output = vec![-19.26937333f64, -19.25906120, -19.27203651];
+        assert_eq!(Outcar::parse_toten_z(&input), output);
+    }
+
+    #[test]
+    fn test_parse_cputime() {
+        let input = r#"
+      LOOP:  cpu time    0.0894: real time    0.0949
+      LOOP:  cpu time    0.0360: real time    0.0330
+      LOOP:  cpu time    0.0275: real time    0.0261
+     LOOP+:  cpu time    2.0921: real time    2.0863
+     LOOP+:  cpu time    1.2021: real time    1.1865
+     LOOP+:  cpu time    1.2788: real time    1.2670"#;
+        let output = vec![2.0863, 1.1865, 1.2670];
+        assert_eq!(Outcar::parse_cputime(&input), output);
+    }
+
+    #[test]
+    fn test_parse_posforce_single_iteration() {
+        let input = r#" POSITION                                       TOTAL-FORCE (eV/Angst)
+ -----------------------------------------------------------------------------------
+      3.87720      4.01520      4.00000        -0.438233     -0.328151      0.000000
+      3.00000      2.48290      4.00000         0.000000      0.536218      0.000000
+      2.12280      4.01520      4.00000         0.438233     -0.328151      0.000000
+      3.00000      3.50000      4.00000         0.000000      0.120085      0.000000
+ -----------------------------------------------------------------------------------
+    total drift:                                0.000000     -0.000260     -0.000000 "#;
+        let output = (
+            vec![[3.87720,4.01520,4.00000],
+                 [3.00000,2.48290,4.00000],
+                 [2.12280,4.01520,4.00000],
+                 [3.00000,3.50000,4.00000]]
+            ,
+            vec![[-0.438233, -0.328151, 0.000000],
+                 [ 0.000000,  0.536218, 0.000000],
+                 [ 0.438233, -0.328151, 0.000000],
+                 [ 0.000000,  0.120085, 0.000000]]
+        );
+
+        assert_eq!(Outcar::_parse_posforce_single_iteration(&input), output);
+    }
+
+    #[test]
+    fn test_parse_posforce() {
+        let input = r#"
+ POSITION                                       TOTAL-FORCE (eV/Angst)
+ -----------------------------------------------------------------------------------
+      3.87720      4.01520      4.00000        -0.438233     -0.328151      0.000000
+      3.00000      2.48290      4.00000         0.000000      0.536218      0.000000
+      2.12280      4.01520      4.00000         0.438233     -0.328151      0.000000
+      3.00000      3.50000      4.00000         0.000000      0.120085      0.000000
+ -----------------------------------------------------------------------------------
+--
+ POSITION                                       TOTAL-FORCE (eV/Angst)
+ -----------------------------------------------------------------------------------
+      3.89220      4.01520      4.00000        -0.930834     -0.563415      0.000000
+      3.00000      2.48290      4.00000        -0.006828      0.527001      0.000000
+      2.12280      4.01520      4.00000         0.458533     -0.304111      0.000000
+      3.00000      3.50000      4.00000         0.479129      0.340525      0.000000
+ -----------------------------------------------------------------------------------
+--
+ POSITION                                       TOTAL-FORCE (eV/Angst)
+ -----------------------------------------------------------------------------------
+      3.86220      4.01520      4.00000         0.089245     -0.065055      0.000000
+      3.00000      2.48290      4.00000         0.007618      0.545925      0.000000
+      2.12280      4.01520      4.00000         0.417195     -0.352508      0.000000
+      3.00000      3.50000      4.00000        -0.514057     -0.128362      0.000000
+ -----------------------------------------------------------------------------------
+--"#;
+        let output = (
+            vec![vec![[3.87720,4.01520,4.00000],
+                      [3.00000,2.48290,4.00000],
+                      [2.12280,4.01520,4.00000],
+                      [3.00000,3.50000,4.00000]],
+                 vec![[3.89220,4.01520,4.00000],
+                      [3.00000,2.48290,4.00000],
+                      [2.12280,4.01520,4.00000],
+                      [3.00000,3.50000,4.00000]],
+                 vec![[3.86220,4.01520,4.00000],
+                      [3.00000,2.48290,4.00000],
+                      [2.12280,4.01520,4.00000],
+                      [3.00000,3.50000,4.00000]],
+            ],
+
+            vec![vec![[-0.438233, -0.328151, 0.000000],
+                      [ 0.000000,  0.536218, 0.000000],
+                      [ 0.438233, -0.328151, 0.000000],
+                      [ 0.000000,  0.120085, 0.000000]],
+                 vec![[-0.930834, -0.563415, 0.000000],
+                      [-0.006828,  0.527001, 0.000000],
+                      [ 0.458533, -0.304111, 0.000000],
+                      [ 0.479129,  0.340525, 0.000000]],
+                 vec![[ 0.089245, -0.065055, 0.000000],
+                      [ 0.007618,  0.545925, 0.000000],
+                      [ 0.417195, -0.352508, 0.000000],
+                      [-0.514057, -0.128362, 0.000000]]
+            ]
+        );
+        assert_eq!(Outcar::parse_posforce(&input), output);
+    }
+
+    #[test]
+    fn test_parse_efermi() {
+        let input = " E-fermi :  -0.7865     XC(G=0):  -2.0223     alpha+bet : -0.5051";
+        let output = -0.7865f64;
+        assert_eq!(Outcar::parse_efermi(&input), output);
+    }
+}
