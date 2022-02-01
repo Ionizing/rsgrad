28ee8322be7f0bed268b13f41e9c8df98dc2ab30
Ionizing <ljchen9@mail.ustc.edu.cn>
2022-02-01 23:42:18 +0800

    [lib.rs] re-export symbols

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,19 @@ pub mod vasp_parsers;
 pub mod commands;
 pub mod traits;
 pub mod types;
+
+pub use traits::OptProcess;
+
+pub use vasp_parsers::poscar::{
+    Poscar,
+    PoscarFormatter,
+};
+
+pub use vasp_parsers::outcar::{
+    Outcar,
+    IonicIteration,
+    IonicIterationsFormat,
+    Vibration,
+    Vibrations,
+    Trajectory,
+};
