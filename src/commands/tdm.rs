62abe8ba2f64d1d1d5832e78ac6a67b1f2581920
Ionizing <PeterSmith_9@outlook.com>
2024-03-28 10:53:48 +0800

    [tdm] fix shuffled order introduced by `par_bridge`

--- src/commands/tdm.rs
+++ src/commands/tdm.rs
@@ -237,7 +237,8 @@ I suggest you provide `gamma_half` argument to avoid confusion.");
 
         let tdms = iproduct!(ibands, jbands)
             .filter(|(iband, jband)| iband < jband)
-            .par_bridge()
+            .collect::<Vec<_>>()
+            .into_par_iter()
             .map(|(iband, jband)| {
                 let eig_i = eigs[(ispin, ikpoint, iband)] - efermi;
                 let eig_j = eigs[(ispin, ikpoint, jband)] - efermi;
